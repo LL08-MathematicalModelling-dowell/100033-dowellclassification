ba10eb1ffd7bed19138fe6e1806bea00535b0382
adwaheed1990 <adwaheed1990@gmail.com>
2023-06-30 16:44:02 +0500

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Home from './pages/Home'
+
+function App() {
+  return (
+    <div className='bg-purple-300 w-full h-full'>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
